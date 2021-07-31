477dd6428e04c1d172236533ebe3ff44de106b71
Gowtham118 <saigowthamch@gmail.com>
2021-07-31 12:57:03 +0530

    comm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from './components/nav/Navbar.js';
 import Hero from './components/hero/Hero.js';
-
+import Bestwork from './components/BestWork/Bestwork.js';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Bestwork/>
     </div>
   );
 }
